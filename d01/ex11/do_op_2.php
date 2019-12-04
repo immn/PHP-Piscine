be38501fd7da003c11fd78d3d6f22a4224f7f8ce
Красноперов Александр <krasnoperov@msk.i-teco.ru>
2019-12-04 13:43:30 +0000

    d01ex11

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -1,95 +1,119 @@
 #!/usr/bin/php
 <?php
-	function is_op($check)
+	function ft_atoi($num)
+	{
+		$sign = 1;
+		$i = 0;
+		$res = 0;
+		$num = trim($num);
+		$len = strlen($num);
+		if ($len == 0)
+			return null;
+		if ($num[0] == "-" && $len >= 2)
+		{
+			$sign = -1;
+			$i++;
+		}
+		else if ($num[0] == "+" && $len >= 2)
+			$i++;
+		else if ($num[0] == "+" || $num[0] == "-")
+			return null;
+		while (is_numeric($num[$i]))
+			$res = $res * 10 + $num[$i++];
+		return ($res * $sign);
+	}
+
+	function is_sign($check)
 	{
 		if ($check && strchr("+-*/%", $check))
 			return (1);
 		return (0);
 	}
 
-	function ft_atoi($line)
-	{
-		$sign = 1;
-		$x = 0;
-		$result = 0;
-		if ($line[$x] == '-')
-		{
-			$sign = -1;
-			$x++;
-		}
-		if ($line[$x] == '+')
-			$x++;
-		while (is_numeric($line[$x]))
-			$result = $result * 10 + $line[$x++];
-		return ($result * $sign);
-	}
-
 	if ($argc != 2)
+	{
 		echo "Incorrect Parameters\n";
+		return null;
+	}
+	
+	$i = 0;
+	$str = trim($argv[1]);
+	$str = preg_replace('/ +/', ' ', $str);
+	if ($str && is_numeric($str[$i]) ||
+	(($str[$i] == '-' || $str[$i] == '+') && is_numeric($str[$i + 1])))
+	{
+		$a = ft_atoi($str);
+		$i++;
+	}
 	else
 	{
-		$i = 0;
-		$error = 0;
-		$str = trim($argv[1]);
-		$str = preg_replace('/ +/', ' ', $str);
-		if ($str && is_numeric($str[$i]) ||
-		(($str[$i] == '-' || $str[$i] == '+') && is_numeric($str[$i + 1])))
-		{
-			$a = ft_atoi($str, $error);
-			$i++;
-		}
-		else
-			$error = 1;
-		while (is_numeric($str[$i]))
-			$i++;
-		if ($str[$i] != ' ' && !is_op($str[$i]))
-			$error = 1;
-		if ($error)
-		{
-			echo "Syntax Error\n";
-			return (0);
-		}
-		while ($str[$i] == ' ')
-			$i++;
-		if (is_op($str[$i]))
-			$op = $str[$i++];
-		else
-			$error = 1;
-		while ($str[$i] == ' ')
-			$i++;
-		if (is_numeric($str[$i]) ||
-		(($str[$i] == '-' || $str[$i] == '+') && is_numeric($str[$i + 1])))
-		{
-			$b = ft_atoi(substr($str, $i), $error);
-			$i++;
-		}
-		else
-			$error = 1;
-		while (is_numeric($str[$i]))
-			$i++;
-		while ($str[$i] == ' ')
-			$i++;
-		if ($str[$i])
-			$error = 1;
-		if ($error)
-		{
-			echo "Syntax Error\n";
-			return (0);
-		}
+		echo "Syntax Error\n";
+		return (0);
 	}
-	if (!$error)
+
+	if (strlen($a) == 0)
 	{
-		if ($b == 0 && ($op == "/" || $op == "%"))
-			echo "0 mustn't be a denominator!\n";
-		else if ($op == "+")
-			echo $a + $b."\n";
-		else if ($op == "-")
-			echo $a - $b."\n";
-		else if ($op == "/")
-			echo $a / $b."\n";
-		else if ($op == "*")
-			echo $a * $b."\n";
-		else if ($op == "%")
-			echo $a % $b."\n";
-}
+		echo "Syntax Error\n";
+		return (0);
+	}
+
+	while (is_numeric($str[$i]))
+		$i++;
+
+	if ($str[$i] != ' ' && !is_sign($str[$i]))
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+
+	while ($str[$i] == ' ')
+		$i++;
+
+	if (is_sign($str[$i]))
+		$op = $str[$i++];
+	else
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+
+	while ($str[$i] == ' ')
+		$i++;
+
+	if (is_numeric($str[$i]) ||
+	(($str[$i] == '-' || $str[$i] == '+') && is_numeric($str[$i + 1])))
+	{
+		$b = ft_atoi(substr($str, $i));
+		$i++;
+	}
+	else
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+
+	while (is_numeric($str[$i]))
+		$i++;
+
+	while ($str[$i] == ' ')
+		$i++;
+
+	if ($str[$i])
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+
+	if ($b == 0 && ($op == "/" || $op == "%"))
+		echo "0 mustn't be a denominator!\n";
+	else if ($op == "+")
+		echo $a + $b."\n";
+	else if ($op == "-")
+		echo $a - $b."\n";
+	else if ($op == "/")
+		echo $a / $b."\n";
+	else if ($op == "*")
+		echo $a * $b."\n";
+	else if ($op == "%")
+		echo $a % $b."\n";
 ?>
