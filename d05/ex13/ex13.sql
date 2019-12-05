e5e3fb9637d9ac7d22926f0a61269ce47e278a9a
immnsn <immnsn@hotmail.com>
2019-12-06 00:27:02 +0300

    d05ex12-15

---
+++ d05/ex13/ex13.sql
@@ -0,0 +1,5 @@
+SELECT
+    ROUND(AVG(nb_seats),
+    0) AS "average"
+FROM
+    cinema;
---
+++ d05/ex15/ex15.sql
@@ -0,0 +1,6 @@
+SELECT
+    REVERSE(SUBSTRING(phone_number, 2)) AS "rebmunenohp"
+FROM
+    distrib
+WHERE
+    phone_number LIKE "05%";
