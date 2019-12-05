6feb77b9c6ad649cd7ac69e5c18481ec82a9001b
Красноперов Александр <krasnoperov@msk.i-teco.ru>
2019-12-05 18:35:54 +0300

    d05 09 10 11

--- d05/ex04/ex04.sql
+++ d05/ex04/ex04.sql
@@ -1,9 +1,9 @@
 UPDATE
     ft_table
 SET
-    "creation_date" = DATE_ADD(
-        "creation_date",
+    creation_date = DATE_ADD(
+        creation_date,
         INTERVAL 20 YEAR
     )
 WHERE
-    "id" > 5;
+    id > 5;
