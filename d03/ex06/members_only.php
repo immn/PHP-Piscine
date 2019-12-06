bc789e5dd3a870f0a557c8a86481dd5c22de1d02
Karliah Garth of oldtown <kgarth-o@ox-r3.21-school.ru>
2019-12-06 20:15:41 +0300

    d03 final

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -3,11 +3,11 @@
     $auth_user = "";
     $auth_pass = "";
     if (array_search("PHP_AUTH_USER" ,$a_k) !== false)
-        $auth_user = $_SERVER['PHP_AUTH_USER'];
+        $auth_user = $_SERVER["PHP_AUTH_USER"];
     if (array_search("PHP_AUTH_PW" ,$a_k) !== false)
-        $auth_pass = $_SERVER['PHP_AUTH_PW'];
-	if ($auth_user == 'zaz' &&
-	$auth_pass == 'jaimelespetitsponeys')
+        $auth_pass = $_SERVER["PHP_AUTH_PW"];
+	if ($auth_user == "zaz" &&
+	$auth_pass == "jaimelespetitsponeys")
 	{
 		$pic = base64_encode(file_get_contents("../img/42.png"));
 		echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,";
@@ -16,8 +16,8 @@
 	}
 	else
 	{
-		header('WWW-Authenticate: Basic realm="kgarth-o"');
-		header('HTTP/1.0 401 Unauthorized');
+		header("WWW-Authenticate: Basic realm=\'Member area\'");
+		header("HTTP/1.0 401 Unauthorized");
 		echo "<html><body>That area is accessible for members only";
 		echo "</body></html>\n";
 	}
