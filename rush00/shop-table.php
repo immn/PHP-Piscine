313c00ee1db11290988442c3b02ae563bff8ed0e
Красноперов Александр <krasnoperov@msk.i-teco.ru>
2019-12-08 12:09:05 +0300

    pricebutton

--- rush00/shop-table.php
+++ rush00/shop-table.php
@@ -1,15 +1,4 @@
 <?php
-// $str ="<div class=\"post\">
-// 			<div class=\"icon\">
-// 				<a class=\"iconscale\" href=\"\">
-// 				<img width=\"235\" height=\"235\" src=\"img\giphy.gif\" class=\"attachment-full size-full wp-post-image\" alt=\"\">
-// 				</a>
-// 				<div class=\"catlabel\">
-// 					<a href=\"\">Stranger Things</a>
-// 				</div>
-// 			</div>
-// 		</div>";
-// 	echo($str);
 	$arr = file('db\games.csv');
 	unset($arr[0]);
 	$i = 0;
@@ -19,15 +8,18 @@
 	{
 		$arr[$i] = explode(';', $arr[$i]);
 		$str .= "<div class=\"post\">
-		<div class=\"icon\">
-			<a class=\"iconscale\" href=\"\">
-			<img width=\"235\" height=\"235\" src=\"".$arr[$i][0]."\" class=\"attachment-full size-full wp-post-image\" alt=\"\">
-			</a>
-			<div class=\"catlabel\">
-				<a href=\"\">".$arr[$i][1]."</a>
-				</div>
-			</div>
-		</div>";
+					<div class=\"icon\">
+						<a class=\"iconscale\" href=\"".$arr[$i][2]."\">
+						<img width=\"235\" height=\"235\" src=\"".$arr[$i][0]."\" class=\"attachment-full size-full wp-post-image\" alt=\"\">
+						</a>
+						<div class=\"catlabel\">
+							<a href=\"".$arr[$i][2]."\">".$arr[$i][1]."</a>
+						</div>
+						<div class=\"price\">
+							<a href=\"".$arr[$i][2]."\">".$arr[$i][3]."$"."</a>
+						</div>
+					</div>
+				</div>";
 	}
 	echo($str);
 
